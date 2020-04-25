f2e2c1bb2670f08c5faac23dbed84000ca41f26e
Snitchholly119 <none@none.com>
2020-04-25 05:23:15 +0300

    Added style.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from os import environ
+import wikipedia
+from random import randint
 
 
 app = Flask(__name__)
@@ -10,6 +12,7 @@ app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
 app.config['FLASK_DEBUG'] = environ.get('FLASK_DEBUG')
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 db =SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -21,5 +24,12 @@ from app.models import Locations
 
 @app.route("/")
 def index():
-    location_list = Locations.query.all()
-    return render_template("index.html", locations=location_list)
+    new_destination=randint(1, 40)
+    return render_template("index.html", destination=new_destination)
+
+
+@app.route("/<int:location_id>")
+def location_page(location_id):
+    bg_color =f"{randint(0, 255)}, {randint(0, 255)}, {randint(0, 255)}"
+    current_location = Locations.query.get(location_id)
+    return render_template("location_page.html", location = current_location, bg_color=bg_color)
--- app/models.py
+++ app/models.py
@@ -5,6 +5,9 @@ class Locations(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     location = db.Column(db.String(100), nullable = False)
     country = db.Column(db.String(300), nullable = False)
+    summary = db.Column(db.String(10000), nullable = True)
+    wiki_link = db.Column(db.String(3000), nullable = True)
+    image_link = db.Column(db.String(3000), nullable = True)
 
     def __repr__(self):
         return f"Locations: {self.location} - {self.country}"
